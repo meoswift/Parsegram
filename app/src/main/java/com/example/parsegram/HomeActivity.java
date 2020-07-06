f3fbde7bc266662952ffe1bd31b1db9413d881ba
Linh Tran <ltran10@fb.com>
2020-07-06 16:15:57 -0700

    Save post to database when user click on Post button in HomeActivity

--- app/src/main/java/com/example/parsegram/HomeActivity.java
+++ app/src/main/java/com/example/parsegram/HomeActivity.java
@@ -4,13 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 import java.util.List;
 
@@ -31,8 +34,44 @@ public class HomeActivity extends AppCompatActivity {
         mCaptureBtn = findViewById(R.id.btnCaptureImage);
         mPostBtn = findViewById(R.id.btnPost);
 
-        queryPosts();
+//        queryPosts();
     }
+
+    // When user click on post, gets description and saves post to database
+    public void onPostClicked(View view) {
+        String description = mDescriptionEt.getText().toString();
+        if (description.isEmpty()) {
+            Toast.makeText(this, "Description cannot be empty.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Get current user and save new post
+        ParseUser currentUser = ParseUser.getCurrentUser();
+        savePost(description, currentUser);
+    }
+
+    // Function to save a new post to Parse database
+    private void savePost(String description, ParseUser currentUser) {
+        // create a new Post that's already a Parse object
+        Post post = new Post();
+        // set required properties of the post
+        post.setUser(currentUser);
+        post.setDescription(description);
+        // save post to database
+        post.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e != null) {
+                    Log.d("HomeActivity", "Error saving post to dtb" + e);
+                }
+
+                Toast.makeText(HomeActivity.this, "Post saved successfully!",
+                        Toast.LENGTH_SHORT).show();
+                mDescriptionEt.setText(null); // clear description box once saved
+            }
+        });
+    }
+
 
     // Finding all objects of class Post in Parse database
     private void queryPosts() {
